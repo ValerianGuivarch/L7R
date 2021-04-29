eacad005c900018962b2277d8857f8f1fd265af9
Anonymous <anon@ymo.us>
2021-04-29 13:15:34 +0200

    update index template and remove some unused files

--- LesSeptRois/settings.py
+++ LesSeptRois/settings.py
@@ -34,7 +34,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'applsr.apps.ApplsrConfig'
+    'applsr.apps.ApplsrConfig',
 ]
 
 MIDDLEWARE = [
