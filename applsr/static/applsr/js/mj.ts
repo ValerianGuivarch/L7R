32e5862e019e5f8ed42be0eabbfc9cd9ceaa1575
Anonymous <anon@ymo.us>
2021-05-02 21:39:13 +0200

    convert mj to ts

--- applsr/static/applsr/js/mj.ts
+++ applsr/static/applsr/js/mj.ts
@@ -1,24 +1,30 @@
-"use strict";
-var nbPJ = 0;
-var listPJ = [];
-var table = "";
+type Stat = "pv" | "arcanes" | "dettes" | "pf" | "pp";
+
+let nbPJ = 0;
+let listPJ: string[] = [];
+let table = "";
+
 nompj = "mj";
 display_secret = true; // override value from lsr.js
-document.addEventListener("DOMContentLoaded", function () {
-    setInterval(function () {
+
+document.addEventListener("DOMContentLoaded", () => {
+    setInterval(() => {
         afficherPJ();
     }, 2000);
+
     afficherPJ();
 });
-function modif(nom, stat, valeur, add) {
+
+function modif(nom: string, stat: Stat, valeur: number, add: boolean) {
     fetch('/mj_interdit_aux_joueurs/modifs_valeurs/' + nom + '/' + stat + '/' + valeur + '/' + add)
-        .catch(function (e) {
-        console.error("error", e);
-    });
-    afficher();
+        .catch(function(e) {
+            console.error("error", e);
+        });
+    afficher()
 }
-function ajouter_pj(name) {
-    var liste_pj = document.querySelector('#liste_pj');
+
+function ajouter_pj(name: string) {
+    let liste_pj = document.querySelector<HTMLElement>('#liste_pj')!;
     table = table +
         '<td align="center">' + '<span id="pj_name"><b>' + name + '</b></span>'
         + '<br/><i><span id="pj_fl_' + name + '"></span></i>'
@@ -34,75 +40,82 @@ function ajouter_pj(name) {
     nbPJ++;
     listPJ.push(name);
 }
+
 function afficherPJ() {
-    listPJ.forEach(function (name, index, array) {
+    listPJ.forEach(function(name, index, array) {
         fetch('/lsr/getcar/' + name)
-            .then(function (response) { return response.text(); })
-            .then(function (json) {
-            var obj = JSON.parse(json);
-            document.querySelector('#pj_pv_' + name).innerHTML = obj.point_de_vie;
-            document.querySelector('#pj_pv_max_' + name).innerHTML = obj.point_de_vie_max;
-            document.querySelector('#pj_pf_' + name).innerHTML = obj.point_de_focus;
-            document.querySelector('#pj_pf_max_' + name).innerHTML = obj.point_de_focus_max;
-            document.querySelector('#pj_pp_' + name).innerHTML = obj.point_de_pouvoir;
-            document.querySelector('#pj_pp_max_' + name).innerHTML = obj.point_de_pouvoir_max;
-            document.querySelector('#pj_dettes_' + name).innerHTML = obj.dettes;
-            document.querySelector('#pj_arcanes_' + name).innerHTML = obj.arcanes;
-            document.querySelector('#pj_arcanes_max_' + name).innerHTML = obj.arcanes_max;
-            document.querySelector('#pj_fl_' + name).innerHTML = obj.fl;
-            document.querySelector('#pj_fu_' + name).innerHTML = obj.fu;
-            document.querySelector('#pj_fs_' + name).innerHTML = obj.fs;
-        }).catch(function (e) {
-            console.error("error", e);
-        });
+            .then((response) => response.text())
+            .then(json => {
+                const obj = JSON.parse(json);
+                document.querySelector('#pj_pv_' + name)!.innerHTML = obj.point_de_vie;
+                document.querySelector('#pj_pv_max_' + name)!.innerHTML = obj.point_de_vie_max;
+                document.querySelector('#pj_pf_' + name)!.innerHTML = obj.point_de_focus;
+                document.querySelector('#pj_pf_max_' + name)!.innerHTML = obj.point_de_focus_max;
+                document.querySelector('#pj_pp_' + name)!.innerHTML = obj.point_de_pouvoir;
+                document.querySelector('#pj_pp_max_' + name)!.innerHTML = obj.point_de_pouvoir_max;
+                document.querySelector('#pj_dettes_' + name)!.innerHTML = obj.dettes;
+                document.querySelector('#pj_arcanes_' + name)!.innerHTML = obj.arcanes;
+                document.querySelector('#pj_arcanes_max_' + name)!.innerHTML = obj.arcanes_max;
+                document.querySelector('#pj_fl_' + name)!.innerHTML = obj.fl;
+                document.querySelector('#pj_fu_' + name)!.innerHTML = obj.fu;
+                document.querySelector('#pj_fs_' + name)!.innerHTML = obj.fs;
+            }).catch(function(e) {
+                console.error("error", e);
+            });
     });
 }
-function effacer_pnj(new_pnj_name) {
-    var pnj = document.querySelector('#pnj_' + new_pnj_name);
+
+function effacer_pnj(new_pnj_name: string) {
+    const pnj = document.querySelector('#pnj_' + new_pnj_name)!;
     pnj.innerHTML = "";
 }
-function modifPNJ(pnj_name, stat, valeur) {
-    var mod = document.querySelector('#pnj_' + stat + '_' + pnj_name);
-    console.log('#pnj_' + stat + '_' + pnj_name);
-    console.log(valeur);
+
+function modifPNJ(pnj_name: string, stat: Stat, valeur: number) {
+    const mod = document.querySelector('#pnj_' + stat + '_' + pnj_name)!;
+    console.log('#pnj_' + stat + '_' + pnj_name)
+    console.log(valeur)
     mod.innerHTML = (parseInt(mod.innerHTML) + valeur).toString();
 }
-function jetPNJ(name, action, stat, pf, pp, ra, sec, dc /** dés cachés */) {
-    var mal = parseInt(document.querySelector('#pnj_mal_' + name).innerHTML);
-    var ben = parseInt(document.querySelector('#pnj_ben_' + name).innerHTML);
-    var opposition = parseInt(document.querySelector('#opposition').value);
-    if (document.querySelector('#opposition_checked').checked) {
-        fetch('/mj/lancer_pnj/' + name + '/' + action + '/' + stat + '/' + pf + '/' + pp + '/' + ra + '/' + mal + '/' + ben + '/' + sec + '/' + dc + '/' + opposition).then(function (response) {
-            response.text().then(function (text) {
+
+function jetPNJ(name: string, action: RollType, stat: Stat, pf: boolean, pp: boolean, ra: boolean, sec: boolean, dc: boolean /** dés cachés */) {
+    const mal = parseInt(document.querySelector('#pnj_mal_' + name)!.innerHTML);
+    const ben = parseInt(document.querySelector('#pnj_ben_' + name)!.innerHTML);
+    const opposition = parseInt(document.querySelector<HTMLInputElement>('#opposition')!.value);
+
+    if(document.querySelector<HTMLInputElement>('#opposition_checked')!.checked) {
+        fetch('/mj/lancer_pnj/' + name + '/' + action + '/' + stat + '/' + pf + '/' + pp + '/' + ra + '/' + mal + '/' + ben + '/' + sec + '/' + dc + '/' + opposition).then(function(response) {
+            response.text().then(function(text) {
                 console.log(text);
-                var degats = parseInt(text);
+                const degats = parseInt(text);
                 console.log(degats);
                 modifPNJ(name, "pv", degats * -1);
             });
         });
-    }
-    else {
+    } else {
         fetch('/mj/lancer_pnj/' + name + '/' + action + '/' + stat + '/' + pf + '/' + pp + '/' + ra + '/' + mal + '/' + ben + '/' + sec + '/' + dc + '/0');
     }
-    if (action == 'JM')
+    if(action == 'JM')
         modifPNJ(name, 'dettes', 1);
-    if (pf)
+    if(pf)
         modifPNJ(name, 'pf', -1);
-    if (pp) {
+    if(pp) {
         modifPNJ(name, 'pp', -1);
         modifPNJ(name, 'dettes', 1);
     }
 }
-function ajouter_pnj(new_pnj_name, new_pnj_chair, new_pnj_esprit, new_pnj_essence, new_pnj_pv_max, new_pnj_pf_max, new_pnj_pp_max) {
-    var new_pnj_dettes = Math.floor(Math.random() * Math.floor(5));
-    var liste_pnj = document.querySelector('#liste_pnj');
-    if (new_pnj_pv_max == "PVmax") {
+
+
+
+function ajouter_pnj(new_pnj_name: string, new_pnj_chair: string, new_pnj_esprit: string, new_pnj_essence: string, new_pnj_pv_max: number | "PVmax", new_pnj_pf_max: number | "PFmax", new_pnj_pp_max: number | "PPmax") {
+    const new_pnj_dettes = Math.floor(Math.random() * Math.floor(5));
+    const liste_pnj = document.querySelector('#liste_pnj')!;
+    if(new_pnj_pv_max == "PVmax") {
         new_pnj_pv_max = parseInt(new_pnj_chair) * 2;
     }
-    if (new_pnj_pf_max == "PFmax") {
+    if(new_pnj_pf_max == "PFmax") {
         new_pnj_pf_max = parseInt(new_pnj_esprit);
     }
-    if (new_pnj_pp_max == "PPmax") {
+    if(new_pnj_pp_max == "PPmax") {
         new_pnj_pp_max = parseInt(new_pnj_essence);
     }
     liste_pnj.innerHTML = liste_pnj.innerHTML
@@ -126,8 +139,12 @@ function ajouter_pnj(new_pnj_name, new_pnj_chair, new_pnj_esprit, new_pnj_essenc
         new_pnj_name + '\',\'mal\',-1);\" >-</button>Mal = <span id=\"pnj_mal_' + new_pnj_name + '\">0</span><button onclick=\"modifPNJ(\'' + new_pnj_name +
         '\',\'mal\',1);\" >+</button> <button onclick=\"modifPNJ(\'' + new_pnj_name + '\',\'ben\',-1);\" >-</button>Ben = <span id=\"pnj_ben_'
         + new_pnj_name + '\">0</span><button onclick=\"modifPNJ(\'' + new_pnj_name + '\',\'ben\',1);\" >+</button> <button onclick=\"modifPNJ(\'' + new_pnj_name + '\',\'dettes\',-1);\" >-</button>Dettes = <span id=\"pnj_dettes_'
-        + new_pnj_name + '\">' + new_pnj_dettes + '</span><button onclick=\"modifPNJ(\'' + new_pnj_name + '\',\'dettes\',1);\" >+</button></span>';
+        + new_pnj_name + '\">' + new_pnj_dettes + '</span><button onclick=\"modifPNJ(\'' + new_pnj_name + '\',\'dettes\',1);\" >+</button></span>'
 }
+
 function effacerLancersDes() {
     fetch('/mj_interdit_aux_joueurs/effacerLancersDes');
 }
+
+
+
