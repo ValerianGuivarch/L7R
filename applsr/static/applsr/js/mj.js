15858ddcfb0d116189788679bec68d70bb2af9ea
Anonymous <anon@ymo.us>
2021-05-09 14:38:43 +0200

    remove some unused code, problem with hidden dice

--- applsr/static/applsr/js/mj.js
+++ applsr/static/applsr/js/mj.js
@@ -1,67 +1,6 @@
 "use strict";
 nompj = "mj";
 display_secret = true; // override value from lsr.js
-function modif(nom, stat, valeur, add) {
-    fetch('/mj_interdit_aux_joueurs/modifs_valeurs/' + nom + '/' + stat + '/' + valeur + '/' + add).catch(function (e) {
-        console.error("error", e);
-    }).then(function () { return afficherPJ(); });
-}
-// to del
-function ajouter_pj2(name) {
-    var _a;
-    var option = document.querySelector('#pj-select option[value="' + name + '"]');
-    (_a = option === null || option === void 0 ? void 0 : option.parentElement) === null || _a === void 0 ? void 0 : _a.removeChild(option); // since we can't add the same PC twice we might as well remove it from the drop down list
-    var pcElement = document.createElement("div");
-    pcElement.className = "pc";
-    pcElement.innerHTML = '<div class="name line">' + name + '</div>'
-        + '<div class="line"><i><span class="pj_fl" id="pj_fl_' + name + '">&nbsp;</span></i></div>'
-        + '<div class="line"><i><span class="pj_fu" id="pj_fu_' + name + '">&nbsp;</span></i></div>'
-        + '<div class="line"><i><span class="pj_fs" id="pj_fs_' + name + '">&nbsp;</span></i></div>'
-        + '<div class="btn-group line">'
-        + '<button class="btn" onclick="modif(\'' + name + '\',\'pv\',1, false);">-</button>'
-        + '<button class="btn minmax" onclick="modif(\'' + name + '\',\'pv_max\',1, false);">--</button>'
-        + '<span class="btn btn-info">PV = <span class="pj_pv" id="pj_pv_' + name + '">3</span>/<span class="pj_pv_max" id="pj_pv_max_' + name + '">6</span></span>'
-        + '<button class="btn minmax" onclick="modif(\'' + name + '\',\'pv_max\',1, true);">++</button>'
-        + '<button class="btn" onclick="modif(\'' + name + '\',\'pv\',1, true);">+</button>'
-        + '</div>'
-        + '<div class="btn-group line">'
-        + '<button class="btn" onclick="modif(\'' + name + '\',\'pf\',1, false);">-</button>'
-        + '<button class="btn minmax" onclick="modif(\'' + name + '\',\'pf_max\',1, false);">--</button>'
-        + '<span class="btn btn-info">PF = <span class="pj_pf" id="pj_pf_' + name + '">3</span>/<span class="pj_pf_max" id="pj_pf_max_' + name + '">6</span></span>'
-        + '<button class="btn minmax" onclick="modif(\'' + name + '\',\'pf_max\',1, true);">++</button>'
-        + '<button class="btn" onclick="modif(\'' + name + '\',\'pf\',1, true);">+</button>'
-        + '</div>'
-        + '<div class="btn-group line">'
-        + '<button class="btn" onclick="modif(\'' + name + '\',\'pp\',1, false);">-</button>'
-        + '<button class="btn minmax" onclick="modif(\'' + name + '\',\'pp_max\',1, false);">--</button>'
-        + '<span class="btn btn-info">PP = <span class="pj_pp" id="pj_pp_' + name + '">3</span>/<span class="pj_pp_max" id="pj_pp_max_' + name + '">6</span></span>'
-        + '<button class="btn minmax" onclick="modif(\'' + name + '\',\'pp_max\',1, true);">++</button>'
-        + '<button class="btn" onclick="modif(\'' + name + '\',\'pp\',1, true);">+</button>'
-        + '</div>'
-        + '<div class="btn-group line">'
-        + '<button class="btn" onclick="modif(\'' + name + '\',\'dettes\',1, false);">-</button>'
-        + '<span class="btn btn-info">DT = <span class="pj_dettes" id="pj_dettes_' + name + '">9</span></span>'
-        + '<button class="btn" onclick="modif(\'' + name + '\',\'dettes\',1, true);">+</button>'
-        + '</div>'
-        + '<div class="btn-group line">'
-        + '<button class="btn" onclick="modif(\'' + name + '\',\'arcanes\',1, false);">-</button>'
-        + '<button class="btn minmax" onclick="modif(\'' + name + '\',\'arcanes_max\',1, false);">--</button>'
-        + '<span class="btn btn-info">AK = <span class="pj_arcanes" id="pj_arcanes_' + name + '">3</span>/<span class="pj_arcanes_max" id="pj_arcanes_max_' + name + '">6</span></span>'
-        + '<button class="btn minmax" onclick="modif(\'' + name + '\',\'arcanes_max\',1, true);">++</button>'
-        + '<button class="btn" onclick="modif(\'' + name + '\',\'arcanes\',1, true);">+</button>'
-        + '</div>';
-    var liste_pj = document.querySelector('#liste_pj');
-    liste_pj.appendChild(pcElement);
-    afficherPJ();
-}
-function ajouter_pj(name) {
-    var _a;
-    var option = document.querySelector('#pj-select option[value="' + name + '"]');
-    (_a = option === null || option === void 0 ? void 0 : option.parentElement) === null || _a === void 0 ? void 0 : _a.removeChild(option); // since we can't add the same PC twice we might as well remove it from the drop down list
-    var liste_pj = document.querySelector('#liste_pj');
-    liste_pj.appendChild(createCharacter(name));
-    updateCharactersOnPage();
-}
 function afficherPJ() {
     var liste_pj = document.querySelector('#liste_pj');
     liste_pj.childNodes.forEach(function (pcNode) {
@@ -116,10 +55,6 @@ function effacer_pnj(pnjElement) {
         }, 5000);
         (_a = pnjElement.parentElement) === null || _a === void 0 ? void 0 : _a.removeChild(pnjElement);
     }
-}
-function modifPNJ(pnjElement, stat, valeur) {
-    var mod = pnjElement.querySelector("." + stat);
-    mod.innerHTML = (parseInt(mod.innerHTML) + valeur).toString();
 }
 function jetPNJ(c, action, dc /** dés cachés */, parentRollId) {
     if (parentRollId === void 0) { parentRollId = null; }
@@ -181,9 +116,6 @@ function jetPNJ(c, action, dc /** dés cachés */, parentRollId) {
         c.debt.current += 1;
     }
 }
-function createJetPnjTemplate(new_pnj_name, new_pnj_stat_value, new_pnj_stat_name, action) {
-    return '<button class="btn" onclick="jetPNJ(this.closest(\'.pnj\'),\'' + action + '\',' + new_pnj_stat_value + ',this.closest(\'.pnj\').querySelector(\'.use_pf\').checked,this.closest(\'.pnj\').querySelector(\'.use_pp\').checked,this.closest(\'.pnj\').querySelector(\'.use_ra\').checked, this.closest(\'.pnj\').querySelector(\'.use_sc\').checked, this.closest(\'.pnj\').querySelector(\'.use_dc\').checked);">' + new_pnj_stat_name + '</button>';
-}
 function ajouter_pnj(new_pnj_name, new_pnj_chair, new_pnj_esprit, new_pnj_essence, new_pnj_pv_max, new_pnj_pf_max, new_pnj_pp_max) {
     var new_pnj_dettes = Math.floor(Math.random() * Math.floor(5));
     var liste_pnj = document.querySelector('#liste_pnj');
@@ -222,92 +154,6 @@ function ajouter_pnj(new_pnj_name, new_pnj_chair, new_pnj_esprit, new_pnj_essenc
     c.power.max = parseInt(new_pnj_pp_max);
     c.debt.current = new_pnj_dettes;
     liste_pnj.appendChild(pnjElement);
-}
-// to del
-function ajouter_pnj2(new_pnj_name, new_pnj_chair, new_pnj_esprit, new_pnj_essence, new_pnj_pv_max, new_pnj_pf_max, new_pnj_pp_max) {
-    var new_pnj_dettes = Math.floor(Math.random() * Math.floor(5));
-    var liste_pnj = document.querySelector('#liste_pnj');
-    if (new_pnj_name == "") {
-        new_pnj_name = "Name";
-    }
-    if (new_pnj_chair == "") {
-        new_pnj_chair = "2";
-    }
-    if (new_pnj_esprit == "") {
-        new_pnj_esprit = new_pnj_chair;
-    }
-    if (new_pnj_essence == "") {
-        new_pnj_essence = new_pnj_chair;
-    }
-    if (new_pnj_pv_max == "") {
-        new_pnj_pv_max = "" + parseInt(new_pnj_chair) * 2;
-    }
-    if (new_pnj_pf_max == "") {
-        new_pnj_pf_max = new_pnj_esprit;
-    }
-    if (new_pnj_pp_max == "") {
-        new_pnj_pp_max = new_pnj_essence;
-    }
-    var pnjElement = document.createElement("div");
-    pnjElement.innerHTML = '<div class="pnj" id="pnj_' + new_pnj_name + '" data-jc="' + new_pnj_chair + '" data-js="' + new_pnj_esprit + '" data-je="' + new_pnj_essence + '">'
-        + '<input type="radio" name="resist" />'
-        + '<button class="btn btn-danger delete-npc" onclick="effacer_pnj(this.closest(\'.pnj\'));"> X </button>'
-        + '<span><b class="name">' + new_pnj_name + '</b> : </span>'
-        + '<span class="btn-group">'
-        + '<button class="btn" onclick="modifPNJ(this.closest(\'.pnj\'),\'pv\',-1);">-</button>'
-        + '<span class="btn btn-info">'
-        + 'PV = <span class="pv" id="pnj_pv_' + new_pnj_name + '">' + new_pnj_pv_max
-        + '</span>/<span class="pv_max" id="pnj_pv_max_' + new_pnj_name + '">' + new_pnj_pv_max + '</span>'
-        + '</span>'
-        + '<button class="btn" onclick="modifPNJ(this.closest(\'.pnj\'),\'pv\',1);">+</button>'
-        + '</span>'
-        + '<span class="btn-group">'
-        + '<button class="btn" onclick="modifPNJ(this.closest(\'.pnj\'),\'pf\',-1);">-</button>'
-        + '<span class="btn btn-info">'
-        + '<input class="use_pf" id="use_pf_' + new_pnj_name + '" type="checkbox" autocomplete="off">'
-        + 'PF: <span class="pf" id="pnj_pf_' + new_pnj_name + '">' + new_pnj_pf_max + '</span>/' + new_pnj_pf_max
-        + '</span>'
-        + '<button class="btn" onclick="modifPNJ(this.closest(\'.pnj\'),\'pf\',1);">+</button>'
-        + '</span>'
-        + '<span class="btn-group">'
-        + '<button class="btn" onclick="modifPNJ(this.closest(\'.pnj\'),\'pp\',-1);">-</button>'
-        + '<span class="btn btn-info">'
-        + '<input class="use_pp" id="use_pp_' + new_pnj_name + '" type="checkbox" autocomplete="off">'
-        + 'PP: <span class="pp" id="pnj_pp_' + new_pnj_name + '">' + new_pnj_pp_max + '</span>/' + new_pnj_pp_max
-        + '</span>'
-        + '<button class="btn" onclick="modifPNJ(this.closest(\'.pnj\'),\'pp\',1);">+</button>'
-        + '</span>'
-        + '<label class="btn">'
-        + '<input class="use_ra" id="use_ra_' + new_pnj_name + '" type="checkbox" autocomplete="off"> RA'
-        + '</label>'
-        + '<label class="btn">'
-        + '<input class="use_sc" id="use_sc_' + new_pnj_name + '" type="checkbox" autocomplete="off"> Secret'
-        + '</label>'
-        + '<label class="btn">'
-        + '<input class="use_dc" id="use_dc_' + new_pnj_name + '" type="checkbox" checked="true" autocomplete="off"> Cachés'
-        + '</label>'
-        + createJetPnjTemplate(new_pnj_name, new_pnj_chair, "Chair", "JC")
-        + createJetPnjTemplate(new_pnj_name, new_pnj_esprit, "Esprit", "JS")
-        + createJetPnjTemplate(new_pnj_name, new_pnj_essence, "Essence", "JE")
-        + createJetPnjTemplate(new_pnj_name, new_pnj_essence, "Magie", "JM")
-        + '<span class="btn-group">'
-        + '<button class="btn btn-danger" onclick="modifPNJ(this.closest(\'.pnj\'),\'mal\',-1);">-</button>'
-        + '<span class="btn btn-outline-danger">Mal: <span class="mal" id="pnj_mal_' + new_pnj_name + '">0</span></span>'
-        + '<button class="btn btn-danger" onclick="modifPNJ(this.closest(\'.pnj\'),\'mal\',1);">+</button> '
-        + '</span>'
-        + '<span class="btn-group">'
-        + '<button class="btn btn-success" onclick="modifPNJ(this.closest(\'.pnj\'),\'ben\',-1);">-</button>'
-        + '<span class="btn btn-outline-success">Ben: <span class="ben" id="pnj_ben_' + new_pnj_name + '">0</span></span>'
-        + '<button class="btn btn-success" onclick="modifPNJ(this.closest(\'.pnj\'),\'ben\',1);">+</button> '
-        + '</span>'
-        + '<span class="btn-group">'
-        + '<button class="btn" onclick="modifPNJ(this.closest(\'.pnj\'),\'dettes\',-1);">-</button>'
-        + '<span class="btn btn-info">Dettes: <span class="dettes" id="pnj_dettes_' + new_pnj_name + '">' + new_pnj_dettes + '</span></span>'
-        + '<button class="btn" onclick="modifPNJ(this.closest(\'.pnj\'),\'dettes\',1);">+</button>'
-        + '</span>'
-        + ' <input type="text" />'
-        + '</div>';
-    liste_pnj.appendChild(pnjElement.firstChild);
 }
 function effacerLancersDes() {
     fetch('/mj_interdit_aux_joueurs/effacerLancersDes').then(function () { return afficher(nompj); });
--- applsr/static/applsr/js/mj.ts
+++ applsr/static/applsr/js/mj.ts
@@ -3,69 +3,6 @@ type Stat = "pv" | "arcanes" | "dettes" | "pf" | "pp" | "ben" | "mal";
 nompj = "mj";
 display_secret = true; // override value from lsr.js
 
-function modif(nom: string, stat: Stat, valeur: number, add: boolean) {
-    fetch('/mj_interdit_aux_joueurs/modifs_valeurs/' + nom + '/' + stat + '/' + valeur + '/' + add).catch(function(e) {
-        console.error("error", e);
-    }).then(() => afficherPJ());
-}
-
-// to del
-function ajouter_pj2(name: string) {
-    const option = document.querySelector('#pj-select option[value="' + name + '"]');
-    option?.parentElement?.removeChild(option); // since we can't add the same PC twice we might as well remove it from the drop down list
-
-    const pcElement = document.createElement("div");
-    pcElement.className = "pc";
-    pcElement.innerHTML = '<div class="name line">' + name + '</div>'
-        + '<div class="line"><i><span class="pj_fl" id="pj_fl_' + name + '">&nbsp;</span></i></div>'
-        + '<div class="line"><i><span class="pj_fu" id="pj_fu_' + name + '">&nbsp;</span></i></div>'
-        + '<div class="line"><i><span class="pj_fs" id="pj_fs_' + name + '">&nbsp;</span></i></div>'
-        + '<div class="btn-group line">'
-            + '<button class="btn" onclick="modif(\'' + name + '\',\'pv\',1, false);">-</button>'
-            + '<button class="btn minmax" onclick="modif(\'' + name + '\',\'pv_max\',1, false);">--</button>'
-            + '<span class="btn btn-info">PV = <span class="pj_pv" id="pj_pv_' + name + '">3</span>/<span class="pj_pv_max" id="pj_pv_max_' + name + '">6</span></span>'
-            + '<button class="btn minmax" onclick="modif(\'' + name + '\',\'pv_max\',1, true);">++</button>'
-            + '<button class="btn" onclick="modif(\'' + name + '\',\'pv\',1, true);">+</button>'
-        + '</div>'
-        + '<div class="btn-group line">'
-            + '<button class="btn" onclick="modif(\'' + name + '\',\'pf\',1, false);">-</button>'
-            + '<button class="btn minmax" onclick="modif(\'' + name + '\',\'pf_max\',1, false);">--</button>'
-            + '<span class="btn btn-info">PF = <span class="pj_pf" id="pj_pf_' + name + '">3</span>/<span class="pj_pf_max" id="pj_pf_max_' + name + '">6</span></span>'
-            + '<button class="btn minmax" onclick="modif(\'' + name + '\',\'pf_max\',1, true);">++</button>'
-            + '<button class="btn" onclick="modif(\'' + name + '\',\'pf\',1, true);">+</button>'
-        + '</div>'
-        + '<div class="btn-group line">'
-            + '<button class="btn" onclick="modif(\'' + name + '\',\'pp\',1, false);">-</button>'
-            + '<button class="btn minmax" onclick="modif(\'' + name + '\',\'pp_max\',1, false);">--</button>'
-            + '<span class="btn btn-info">PP = <span class="pj_pp" id="pj_pp_' + name + '">3</span>/<span class="pj_pp_max" id="pj_pp_max_' + name + '">6</span></span>'
-            + '<button class="btn minmax" onclick="modif(\'' + name + '\',\'pp_max\',1, true);">++</button>'
-            + '<button class="btn" onclick="modif(\'' + name + '\',\'pp\',1, true);">+</button>'
-        + '</div>'
-        + '<div class="btn-group line">'
-            + '<button class="btn" onclick="modif(\'' + name + '\',\'dettes\',1, false);">-</button>'
-            + '<span class="btn btn-info">DT = <span class="pj_dettes" id="pj_dettes_' + name + '">9</span></span>'
-            + '<button class="btn" onclick="modif(\'' + name + '\',\'dettes\',1, true);">+</button>'
-        + '</div>'
-        + '<div class="btn-group line">'
-            + '<button class="btn" onclick="modif(\'' + name + '\',\'arcanes\',1, false);">-</button>'
-            + '<button class="btn minmax" onclick="modif(\'' + name + '\',\'arcanes_max\',1, false);">--</button>'
-            + '<span class="btn btn-info">AK = <span class="pj_arcanes" id="pj_arcanes_' + name + '">3</span>/<span class="pj_arcanes_max" id="pj_arcanes_max_' + name + '">6</span></span>'
-            + '<button class="btn minmax" onclick="modif(\'' + name + '\',\'arcanes_max\',1, true);">++</button>'
-            + '<button class="btn" onclick="modif(\'' + name + '\',\'arcanes\',1, true);">+</button>'
-        + '</div>';
-    const liste_pj = document.querySelector<HTMLElement>('#liste_pj')!;
-    liste_pj.appendChild(pcElement);
-    afficherPJ();
-}
-
-function ajouter_pj(name: string) {
-    const option = document.querySelector('#pj-select option[value="' + name + '"]');
-    option?.parentElement?.removeChild(option); // since we can't add the same PC twice we might as well remove it from the drop down list
-    const liste_pj = document.querySelector<HTMLElement>('#liste_pj')!;
-    liste_pj.appendChild(createCharacter(name));
-    updateCharactersOnPage();
-}
-
 function afficherPJ() {
     const liste_pj = document.querySelector<HTMLElement>('#liste_pj')!;
     liste_pj.childNodes.forEach(function(pcNode) {
@@ -123,11 +60,6 @@ function effacer_pnj(pnjElement: HTMLElement) {
     }
 }
 
-function modifPNJ(pnjElement: HTMLElement, stat: Stat, valeur: number) {
-    const mod = pnjElement.querySelector("." + stat)!;
-    mod.innerHTML = (parseInt(mod.innerHTML) + valeur).toString();
-}
-
 function jetPNJ(c: LocalCharacterView, action: RollType, dc: boolean /** dés cachés */, parentRollId: string | null = null) {
     const opposition = parseInt(document.querySelector<HTMLInputElement>('#opposition')!.value);
 
@@ -168,10 +100,6 @@ function jetPNJ(c: LocalCharacterView, action: RollType, dc: boolean /** dés ca
         c.power.current -= 1;
         c.debt.current += 1;
     }
-}
-
-function createJetPnjTemplate(new_pnj_name: string, new_pnj_stat_value: string, new_pnj_stat_name: string, action: RollTypeBackend) {
-    return '<button class="btn" onclick="jetPNJ(this.closest(\'.pnj\'),\'' + action + '\',' + new_pnj_stat_value + ',this.closest(\'.pnj\').querySelector(\'.use_pf\').checked,this.closest(\'.pnj\').querySelector(\'.use_pp\').checked,this.closest(\'.pnj\').querySelector(\'.use_ra\').checked, this.closest(\'.pnj\').querySelector(\'.use_sc\').checked, this.closest(\'.pnj\').querySelector(\'.use_dc\').checked);">' + new_pnj_stat_name + '</button>'
 }
 
 function ajouter_pnj(new_pnj_name: string, new_pnj_chair: string, new_pnj_esprit: string, new_pnj_essence: string, new_pnj_pv_max: string | "PVmax", new_pnj_pf_max: string | "PFmax", new_pnj_pp_max: string | "PPmax") {
@@ -219,114 +147,6 @@ function ajouter_pnj(new_pnj_name: string, new_pnj_chair: string, new_pnj_esprit
     c.debt.current = new_pnj_dettes;
     
     liste_pnj.appendChild(pnjElement);
-}
-
-// to del
-function ajouter_pnj2(new_pnj_name: string, new_pnj_chair: string, new_pnj_esprit: string, new_pnj_essence: string, new_pnj_pv_max: string | "PVmax", new_pnj_pf_max: string | "PFmax", new_pnj_pp_max: string | "PPmax") {
-    const new_pnj_dettes = Math.floor(Math.random() * Math.floor(5));
-    const liste_pnj = document.querySelector('#liste_pnj')!;
-    
-    if(new_pnj_name == "") {
-        new_pnj_name = "Name";
-    }
-
-    if(new_pnj_chair == "") {
-        new_pnj_chair = "2";
-    }
-
-    if(new_pnj_esprit == "") {
-        new_pnj_esprit = new_pnj_chair;
-    }
-
-    if(new_pnj_essence == "") {
-        new_pnj_essence = new_pnj_chair;
-    }
-
-    if(new_pnj_pv_max == "") {
-        new_pnj_pv_max = "" + parseInt(new_pnj_chair) * 2;
-    }
-    if(new_pnj_pf_max == "") {
-        new_pnj_pf_max = new_pnj_esprit;
-    }
-    if(new_pnj_pp_max == "") {
-        new_pnj_pp_max = new_pnj_essence;
-    }
-    
-    
-    const pnjElement = document.createElement("div");
-    pnjElement.innerHTML = '<div class="pnj" id="pnj_' + new_pnj_name + '" data-jc="' + new_pnj_chair + '" data-js="' + new_pnj_esprit + '" data-je="' + new_pnj_essence + '">'
-
-        + '<input type="radio" name="resist" />'
-        + '<button class="btn btn-danger delete-npc" onclick="effacer_pnj(this.closest(\'.pnj\'));"> X </button>'
-        + '<span><b class="name">' + new_pnj_name + '</b> : </span>'
-        
-        + '<span class="btn-group">'
-            + '<button class="btn" onclick="modifPNJ(this.closest(\'.pnj\'),\'pv\',-1);">-</button>'
-            + '<span class="btn btn-info">'
-                + 'PV = <span class="pv" id="pnj_pv_' + new_pnj_name + '">' + new_pnj_pv_max
-                + '</span>/<span class="pv_max" id="pnj_pv_max_' + new_pnj_name + '">' + new_pnj_pv_max + '</span>'
-            + '</span>'
-            + '<button class="btn" onclick="modifPNJ(this.closest(\'.pnj\'),\'pv\',1);">+</button>'
-        + '</span>'
-        
-        + '<span class="btn-group">'
-            + '<button class="btn" onclick="modifPNJ(this.closest(\'.pnj\'),\'pf\',-1);">-</button>'
-            + '<span class="btn btn-info">'
-                + '<input class="use_pf" id="use_pf_' + new_pnj_name + '" type="checkbox" autocomplete="off">'
-                + 'PF: <span class="pf" id="pnj_pf_' + new_pnj_name + '">' + new_pnj_pf_max + '</span>/' + new_pnj_pf_max
-            + '</span>'
-            + '<button class="btn" onclick="modifPNJ(this.closest(\'.pnj\'),\'pf\',1);">+</button>'
-        + '</span>'
-
-        + '<span class="btn-group">'
-            + '<button class="btn" onclick="modifPNJ(this.closest(\'.pnj\'),\'pp\',-1);">-</button>'
-            + '<span class="btn btn-info">'
-                + '<input class="use_pp" id="use_pp_' + new_pnj_name + '" type="checkbox" autocomplete="off">'
-                + 'PP: <span class="pp" id="pnj_pp_' + new_pnj_name + '">' + new_pnj_pp_max + '</span>/' + new_pnj_pp_max
-            + '</span>'
-            + '<button class="btn" onclick="modifPNJ(this.closest(\'.pnj\'),\'pp\',1);">+</button>'
-        + '</span>'
-
-        + '<label class="btn">'
-            + '<input class="use_ra" id="use_ra_' + new_pnj_name + '" type="checkbox" autocomplete="off"> RA'
-        + '</label>'
-
-        + '<label class="btn">'
-            + '<input class="use_sc" id="use_sc_' + new_pnj_name + '" type="checkbox" autocomplete="off"> Secret'
-        + '</label>'
-
-        + '<label class="btn">'
-            + '<input class="use_dc" id="use_dc_' + new_pnj_name + '" type="checkbox" checked="true" autocomplete="off"> Cachés'
-        + '</label>'
-
-        + createJetPnjTemplate(new_pnj_name, new_pnj_chair, "Chair", "JC")
-        + createJetPnjTemplate(new_pnj_name, new_pnj_esprit, "Esprit", "JS")
-        + createJetPnjTemplate(new_pnj_name, new_pnj_essence, "Essence", "JE")
-        + createJetPnjTemplate(new_pnj_name, new_pnj_essence, "Magie", "JM")
-        
-        + '<span class="btn-group">'
-            + '<button class="btn btn-danger" onclick="modifPNJ(this.closest(\'.pnj\'),\'mal\',-1);">-</button>'
-            + '<span class="btn btn-outline-danger">Mal: <span class="mal" id="pnj_mal_' + new_pnj_name + '">0</span></span>'
-            + '<button class="btn btn-danger" onclick="modifPNJ(this.closest(\'.pnj\'),\'mal\',1);">+</button> '
-        + '</span>'
-        
-        + '<span class="btn-group">'
-            + '<button class="btn btn-success" onclick="modifPNJ(this.closest(\'.pnj\'),\'ben\',-1);">-</button>'
-            + '<span class="btn btn-outline-success">Ben: <span class="ben" id="pnj_ben_' + new_pnj_name + '">0</span></span>'
-            + '<button class="btn btn-success" onclick="modifPNJ(this.closest(\'.pnj\'),\'ben\',1);">+</button> '
-        + '</span>'
-        
-        + '<span class="btn-group">'
-            + '<button class="btn" onclick="modifPNJ(this.closest(\'.pnj\'),\'dettes\',-1);">-</button>'
-            + '<span class="btn btn-info">Dettes: <span class="dettes" id="pnj_dettes_' + new_pnj_name + '">' + new_pnj_dettes + '</span></span>'
-            + '<button class="btn" onclick="modifPNJ(this.closest(\'.pnj\'),\'dettes\',1);">+</button>'
-        + '</span>'
-
-        + ' <input type="text" />'
-
-        + '</div>'
-
-        liste_pnj.appendChild(pnjElement.firstChild!);
 }
 
 function effacerLancersDes() {
